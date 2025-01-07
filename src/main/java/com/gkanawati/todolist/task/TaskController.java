42b9f2e0629ad6af568890d55e87babd252ce9a4
gkanawati <gabrielkanawati3@gmail.com>
2025-01-07 09:47:33 -0300

    feat: Add endpoint to retrieve tasks by user ID in TaskController

--- src/main/java/com/gkanawati/todolist/task/TaskController.java
+++ src/main/java/com/gkanawati/todolist/task/TaskController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,13 @@ public class TaskController {
 
     var createdTask = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
+  }
+
+  @GetMapping("")
+  public ResponseEntity<Object> getAll(HttpServletRequest request) {
+    var userId = (UUID) request.getAttribute("userId");
+    var tasks = this.taskRepository.findByUserId(userId);
+    return ResponseEntity.status(HttpStatus.OK).body(tasks);
   }
 
 }
